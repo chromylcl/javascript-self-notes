a9dd2698650c9b3223cdc8ae3691427ee5bd2819
Parth Atkari <parthatkari6@gmail.com>
2025-10-28 02:42:39 +0530

    Create errorhandling.js

---
+++ errorhandling.js
@@ -0,0 +1,42 @@
+let stocks = {
+  Fruits: ["Strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"]
+};
+
+let is_shop_open = false;
+let order = (time, work) => {
+  return new Promise((resolve, reject) => {
+    if (is_shop_open) {
+      setTimeout(() => {
+        resolve(work());
+      }, time);
+    } else {
+      reject(console.log("Our shop is closed!"));
+    }
+  });
+};
+order(2000, () => console.log(`${stocks.Fruits[0]} was selected!`))
+  .then(() => {
+    return order(0000, () => console.log("Production has started!"));
+  })
+
+  .then(() => {
+    return order(2000, () => console.log("Fruits are chopped!"));
+  })
+
+  .then(() => {
+    return order(1000, () => {
+      console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected!`);
+    });
+  })
+  .then(() => {
+    return order(1000, () => {
+      console.log("Order is ready , Please collect!");
+    });
+  })
+
+  .catch(() => {
+    console.log(`Customer left!`);
+  });
